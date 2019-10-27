350f6116e95e268d767c2b08ff0cc27e14a36df3
neil <ngneilgordon@gmail.com>
2019-10-27 15:34:27 -0400

    add config logout url

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ export default {
     authTokenKey: 'token',
     development: {
         baseUrl: 'http://localhost:5678',
+        logoutUrl: 'http://localhost:5678?logout=true',
     },
-    test: {
-
-    },
+    test: {},
     production: {
-    }
+        logoutUrl: '/?logout=true',
+    },
 }
