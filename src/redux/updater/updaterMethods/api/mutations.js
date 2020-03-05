d9bab9eea2a03149d89f9f5855b0001e0f2a086e
neil <ngneilgordon@gmail.com>
2020-03-04 19:33:19 -0500

    add findByIdAndUpdate mutation updater method

--- src/redux/updater/updaterMethods/api/mutations.js
+++ src/redux/updater/updaterMethods/api/mutations.js
@@ -17,13 +17,32 @@ modelNames.forEach(modelName => {
     generatedMutationUpdaterMethods[mutationName] = function ({ query = {}, updates = {}, successActions = [] }) {
         return {
             updateType: 'api',
-            args: { query, updates, successActions }, 
             serviceOptions: {
                 method: 'post',
                 url: '/graphql',
                 data: {
                     query: `mutation {
                         ${mutationName}(query: ${formatUpdaterObjectArgs(query)}, updates: ${formatUpdaterObjectArgs(updates)}) {
+                            ${Object.keys(updates).join(' ')}
+                        }
+                    }`
+                }
+            },
+            successActions
+        }
+    }
+
+    // findByIdAndUpdate
+    mutationName = `find${modelName}ByIdAndUpdate`
+    generatedMutationUpdaterMethods[mutationName] = function ({ userId, updates = {}, successActions = [] }) {
+        return {
+            updateType: 'api',
+            serviceOptions: {
+                method: 'post',
+                url: '/graphql',
+                data: {
+                    query: `mutation {
+                        ${mutationName}(userId: "${userId}", updates: ${formatUpdaterObjectArgs(updates)}) {
                             ${Object.keys(updates).join(' ')}
                         }
                     }`
