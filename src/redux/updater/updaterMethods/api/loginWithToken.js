dea67c2123d8144fd5a37523e8544506fa5513b8
neil <ngneilgordon@gmail.com>
2020-03-04 19:33:57 -0500

    get social auth  virtuals on login

--- src/redux/updater/updaterMethods/api/loginWithToken.js
+++ src/redux/updater/updaterMethods/api/loginWithToken.js
@@ -12,6 +12,8 @@ export default () => {
                     getUserWithToken {
                         id 
                         emailAddress 
+                        isFacebookAccountConnected
+                        isGoogleAccountConnected
                         ${authTokenKey}
                         firstName 
                         lastName 
