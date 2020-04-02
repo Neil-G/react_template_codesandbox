93e8376e4345d10f39e56adb6ec117820cb51ca0
neil <ngneilgordon@gmail.com>
2020-04-02 15:19:15 -0400

    add messages page skeleton

--- src/components/pages/messages/index.js
+++ src/components/pages/messages/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import { get } from 'lodash'
 import { connect } from 'react-redux'
-import { UserInfoForm } from './../../forms/TextForms'
+import { messagesPageConfig } from './../../../configs/pages'
+
+import Page from './../../shared/Page'
 /*
 |--------------------------------------------------------------------------
 | Styled Components
@@ -20,9 +22,21 @@ const Container = styled.div``
 class MessagesPage extends React.Component {
   render() {
     return (
-      <Container>
-        <h5>Messages</h5>
-      </Container>
+      <Page.Container>
+        <Page.ContentContainer>
+          <Page.Title>Messages</Page.Title>
+          <Page.Nav
+            rootPath={messagesPageConfig.path}
+            navItems={[
+              { label: 'new', count: 10, icon: 'fal fa-inbox' },
+              { label: 'read', count: 25, icon: 'fal fa-check' },
+              { label: 'saved', count: 15, icon: 'fal fa-bookmark' },
+              { label: 'archived', count: 15, icon: 'fal fa-archive' },
+              { label: 'all', count: 5 },
+            ]}
+          />
+        </Page.ContentContainer>
+      </Page.Container>
     )
   }
 }
