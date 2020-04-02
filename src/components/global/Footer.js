b0c67f9d4ff9b5eda3d7c96fccbbcaaa2e3ffdcf
neil <ngneilgordon@gmail.com>
2020-04-02 16:06:41 -0400

    change footer color

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,11 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 const Container = styled.footer`
   height: 200px;
-  background: #512da8;
-`;
+  background: #2b2d42;
+`
 
 export default () => {
-  return <Container />;
-};
+  return <Container />
+}
