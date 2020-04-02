71d754e8332d0ddb3695883b3072dc636772b1cf
neil <ngneilgordon@gmail.com>
2020-04-01 21:58:37 -0400

    add PageNavContainer

--- src/components/shared/Page.js
+++ src/components/shared/Page.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+import { capitalize } from 'lodash'
 import { SEARCH_BAR_DESKTOP_WIDTH } from './../../styles/globalLayout'
 
 /*
@@ -13,7 +16,7 @@ export const PageContainer = styled.div`
     return (
       withSearchPanel &&
       css`
-        padding-left: ${SEARCH_BAR_DESKTOP_WIDTH + 12}px;
+        padding-left: ${SEARCH_BAR_DESKTOP_WIDTH}px;
         @media (max-width: 720px) {
           padding-left: 0px;
         }
@@ -28,7 +31,9 @@ export const PageContainer = styled.div`
 |--------------------------------------------------------------------------
 */
 
-export const PageTitle = styled.h1``
+export const PageTitle = styled.h1`
+  font-size: 24px;
+`
 
 /*
 |--------------------------------------------------------------------------
@@ -39,8 +44,116 @@ export const PageTitle = styled.h1``
 export const PageContentContainer = styled.div`
   margin: auto;
   max-width: 1200px;
-  padding: 12px;
+  padding: 18px;
 `
+
+/*
+|--------------------------------------------------------------------------
+| Page Navigation
+|--------------------------------------------------------------------------
+*/
+
+const PageNavContainer = styled.div`
+  box-shadow: 0px 1px 0px #e3eaef;
+  box-sizing: border-box;
+  margin: 27px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 720px) {
+    margin: 16px 0;
+    flex-direction: column;
+    align-items: baseline;
+    box-shadow: none;
+  }
+`
+const NavItemsContainer = styled.div`
+  display: flex;
+`
+
+const SectionContainer = styled.div`
+  display: flex;
+  @media (max-width: 720px) {
+    width: 100%;
+    margin-bottom: 12px;
+  }
+`
+
+const PageNavItemContainer = styled(SectionContainer)`
+  display: flex;
+  align-items: center;
+  color: #333;
+  padding: 2px 10px 12px;
+  margin-right: 14px;
+  cursor: ${({ isActive }) => isActive && 'default !important'};
+  border-bottom: ${({ isActive }) => isActive && '4px solid #2E595C'};
+  font-size: 12px;
+  @media (max-width: 540px) {
+    font-size: 11px;
+    margin-right: 0;
+  }
+  > a {
+    text-decoration: none !important;
+  }
+`
+
+const TotalCount = styled.div`
+  background: ${({ isActive }) => (isActive ? '#2E595C' : '#8493A7')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  border-radius: 100px;
+  margin-left: 8px;
+  padding: 3px 7px;
+  font-size: 0.85em;
+  @media (max-width: 540px) {
+    margin-left: 3px;
+  }
+`
+
+const PageNavItem = ({ label, isActive, to, count }) => {
+  return (
+    <Link
+      to={to}
+      onClick={() => window.scrollTo(0, 0)}
+      style={{ textDecoration: 'none' }}
+    >
+      <PageNavItemContainer isActive={isActive}>
+        {capitalize(label)} <TotalCount isActive={isActive}>{count}</TotalCount>
+      </PageNavItemContainer>
+    </Link>
+  )
+}
+
+export const PageNav = withRouter(
+  ({
+    location: { pathname },
+    navItems = [],
+    rootPath,
+    renderFilterOptions,
+  }) => {
+    return (
+      <PageNavContainer>
+        {/* New */}
+        <NavItemsContainer>
+          {navItems.map(({ label, count }) => (
+            <PageNavItem
+              key={label}
+              label={label}
+              count={count}
+              to={`${rootPath}/${label.split(' ').join('-')}`}
+              isActive={pathname.includes(label)}
+            />
+          ))}
+        </NavItemsContainer>
+        <SectionContainer>
+          {renderFilterOptions && renderFilterOptions()}
+        </SectionContainer>
+      </PageNavContainer>
+    )
+  },
+)
 
 /*
 |--------------------------------------------------------------------------
@@ -52,4 +165,5 @@ export default {
   Container: PageContainer,
   ContentContainer: PageContentContainer,
   Title: PageTitle,
+  Nav: PageNav,
 }
