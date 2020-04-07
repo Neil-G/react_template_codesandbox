d15ae37d7fe8ff3f27fd35765987bce97cd003c2
neil <ngneilgordon@gmail.com>
2020-04-06 20:32:35 -0400

    add create mutation

--- src/redux/updater/updaterMethods/api/mutations.js
+++ src/redux/updater/updaterMethods/api/mutations.js
@@ -1,56 +1,90 @@
-const modelNames = ['User']
+const modelNames = ['User', 'Item']
 const generatedMutationUpdaterMethods = {}
 
 const formatUpdaterObjectArgs = args => {
-    let formattedArgs = JSON.stringify(args)
-    Object.keys(args).forEach(key => {
-        formattedArgs = formattedArgs.replace(`"${key}"`, key)
-    })
-    return formattedArgs
+  let formattedArgs = JSON.stringify(args)
+  Object.keys(args).forEach(key => {
+    formattedArgs = formattedArgs.replace(`"${key}"`, key)
+  })
+  return formattedArgs
 }
 
 modelNames.forEach(modelName => {
-    let mutationName
+  let mutationName
 
-    // findOneAndUpdate
-    mutationName = `findOne${modelName}AndUpdate`
-    generatedMutationUpdaterMethods[mutationName] = function ({ query = {}, updates = {}, successActions = [] }) {
-        return {
-            updateType: 'api',
-            serviceOptions: {
-                method: 'post',
-                url: '/graphql',
-                data: {
-                    query: `mutation {
-                        ${mutationName}(query: ${formatUpdaterObjectArgs(query)}, updates: ${formatUpdaterObjectArgs(updates)}) {
-                            ${Object.keys(updates).join(' ')}
-                        }
-                    }`
-                }
-            },
-            successActions
-        }
+  // findOneAndUpdate
+  const findOneAndUpdateMutationName = `findOne${modelName}AndUpdate`
+  generatedMutationUpdaterMethods[findOneAndUpdateMutationName] = function({
+    query = {},
+    updates = {},
+    successActions = [],
+  }) {
+    return {
+      updateType: 'api',
+      serviceOptions: {
+        method: 'post',
+        url: '/graphql',
+        data: {
+          query: `mutation {
+                        ${findOneAndUpdateMutationName}(query: ${formatUpdaterObjectArgs(
+            query,
+          )}, updates: ${formatUpdaterObjectArgs(updates)}) {
+                  ${Object.keys(updates).join(' ')}
+              }
+          }`,
+        },
+      },
+      successActions,
     }
+  }
 
-    // findByIdAndUpdate
-    mutationName = `find${modelName}ByIdAndUpdate`
-    generatedMutationUpdaterMethods[mutationName] = function ({ userId, updates = {}, successActions = [] }) {
-        return {
-            updateType: 'api',
-            serviceOptions: {
-                method: 'post',
-                url: '/graphql',
-                data: {
-                    query: `mutation {
-                        ${mutationName}(userId: "${userId}", updates: ${formatUpdaterObjectArgs(updates)}) {
-                            ${Object.keys(updates).join(' ')}
-                        }
-                    }`
-                }
-            },
-            successActions
-        }
+  // findByIdAndUpdate
+  const findByIdAndUpdateMutationName = `find${modelName}ByIdAndUpdate`
+  generatedMutationUpdaterMethods[findByIdAndUpdateMutationName] = function({
+    id,
+    updates = {},
+    successActions = [],
+  }) {
+    return {
+      updateType: 'api',
+      serviceOptions: {
+        method: 'post',
+        url: '/graphql',
+        data: {
+          query: `mutation {
+              ${findByIdAndUpdateMutationName}(id: "${id}", updates: ${formatUpdaterObjectArgs(
+            updates,
+          )}) {
+                id   ${Object.keys(updates).join(' ')}
+              }
+          }`,
+        },
+      },
+      successActions,
     }
+  }
+
+  const createMutationName = `create${modelName}`
+  generatedMutationUpdaterMethods[createMutationName] = function({
+    data = {},
+    successActions = [],
+  }) {
+    return {
+      updateType: 'api',
+      serviceOptions: {
+        method: 'post',
+        url: '/graphql',
+        data: {
+          query: `mutation {
+              ${createMutationName}(data: ${formatUpdaterObjectArgs(data)}) {
+                id  ${Object.keys(data).join(' ')}
+              }
+          }`,
+        },
+      },
+      successActions,
+    }
+  }
 })
 
 export default generatedMutationUpdaterMethods
